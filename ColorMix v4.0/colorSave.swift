418e72b125781b6545ad4e2cd9d1bc14ef710c6b
Anyll <anyllmarkevich@gmail.com>
2019-06-10 18:57:01 -0600

    Added a file to save state of app. It includes a class that keeps the state of sliders and switches, the color, and what view last used these parameters.

---
+++ ColorMix v4.0/colorSave.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  ColorMix v4.0
+//
+//  Created by anyll on 6/10/19.
+//  Copyright © 2019 Anyll Markevich. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class saveState{
+    static var sliders: ViewController.stateStorage = ViewController.stateStorage(rsw: false, rsl: 1, gsw: false, gsl: 1, bsw: false, bsl: 1)
+    static var color: UIColor = .black
+    static var commingFromView: String = "ViewController"
+}
