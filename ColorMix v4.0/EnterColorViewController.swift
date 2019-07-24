b2a78497e11041d6d98032de9c45ba6c9534915c
Anyll <anyllmarkevich@gmail.com>
2019-07-24 11:42:13 -0600

    added new viewController to deal with entered data.

---
+++ ColorMix v4.0/EnterColorViewController.swift
@@ -0,0 +1,34 @@
+//
+//  EnterColorViewController.swift
+//  ColorMix v4.0
+//
+//  Created by anyll on 7/24/19.
+//  Copyright © 2019 Anyll Markevich. All rights reserved.
+//
+
+import UIKit
+
+class EnterColorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var inputText: UITextField!
+    @IBAction func doneEditing(_ sender: Any) {
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
